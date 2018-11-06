5c62f55cf36143029c3fb4e70363c3b54351ce6b
Jose Luis <joseluis@MacBook-Air-de-Jose.local>
2018-11-06 23:41:00 +0100

    Added get cars test

--- test/car.js
+++ test/car.js
@@ -7,45 +7,60 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 /*
-* Test /POST /car/create route
+ * Test /POST create car route
  */
- describe('/POST car', () => {
+describe('/POST car', () => {
     it('it should not POST a car without body', (done) => {
         let car = {};        
-      chai.request(app)
-          .post(conf.CREATE_CAR_ROUTE)
-          .send(car)
-          .end((err, res) => {
+        chai.request(app)
+            .post(conf.CREATE_CAR_ROUTE)
+            .send(car)
+            .end((err, res) => {
                 res.should.have.status(400);
                 res.body.should.have.property('message').eql('Body must not be empty');
-            done();
-          });
+                done();
+            });
     });
     it('it should not POST a car without name field', (done) => {
         let car = {
             field : 'field'
         };        
-      chai.request(app)
-          .post('/car/create')
-          .send(car)
-          .end((err, res) => {
+        chai.request(app)
+            .post(conf.CREATE_CAR_ROUTE)
+            .send(car)
+            .end((err, res) => {
                 res.should.have.status(400);
                 res.body.should.have.property('message').eql('Attribute name not found');
-            done();
-          });
+                done();
+            });
     });
     it('it should POST a car', (done) => {
         let car = {
             name : 'car_name'
         };        
-      chai.request(app)
-          .post('/car/create')
-          .send(car)
-          .end((err, res) => {
+        chai.request(app)
+            .post(conf.CREATE_CAR_ROUTE)
+            .send(car)
+            .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.have.property('_id');
                 res.body.should.have.property('name');
-            done();
-          });
+                done();
+            });
     });
+});
+
+/*
+ * Test /GET get all cars route
+ */
+describe('/GET cars', () => {
+    it('it should GET all the cars', (done) => {
+        chai.request(app)
+            .get(conf.GET_CARS_ROUTE)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                done();
+            });
+    });   
 });
