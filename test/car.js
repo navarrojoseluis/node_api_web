307d988a17d4bcb88a6a66f6f676018617d25f77
Jose Luis <joseluis@MacBook-Air-de-Jose.local>
2018-11-11 18:12:43 +0100

    Added put car test

--- test/car.js
+++ test/car.js
@@ -106,6 +106,56 @@ describe('Cars', () => {
     });
 
     /*
+     * Test /PUT/:id car route
+     */
+    describe('/PUT car', () => {
+        it('it should not UPDATE a car that not exist', (done) => {
+            let car = {
+                field: 'field'
+            };
+            let fake_car_id = '000000000000000000000000';
+            chai.request(app)
+                .put('/car/' + fake_car_id)
+                .send(car)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('message').eql('Car with id ' + fake_car_id + ' not found');
+                    done();
+                });
+        });
+        it('it should not UPDATE a car without body', (done) => {
+            let car = {};
+            let fake_car_id = '000000000000000000000000';
+            chai.request(app)
+                .put('/car/' + fake_car_id)
+                .send(car)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('message').eql('Body must not be empty');
+                    done();
+                });
+        });
+        it('it should UPDATE a car', (done) => {
+            let car = new Car({
+                name: 'carUpdated'
+            });
+            car.save((err, car) => {
+                chai.request(app)
+                    .put('/car/' + car._id)
+                    .send(car)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.have.property('_id');
+                        res.body.should.have.property('name');
+                        done();
+                    });
+            });
+        });
+    });
+
+    /*
      * Test /DELETE/:id car route
      */
     describe('/DELETE/:id car', () => {
