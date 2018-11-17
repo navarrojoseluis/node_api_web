0814b650f42d83d18b4265eaeb8ab6b97a71dfdd
Jose Luis <joseluis@MacBook-Air-de-Jose.local>
2018-11-17 14:05:54 +0100

    Added custom levels to logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -3,22 +3,39 @@ function getLogger(config) {
     const winston = require('winston');
 
     const env = process.env.NODE_ENV || 'development';
-    
+
     const consoleFormat = winston.format.combine(
         winston.format.colorize(),
-        winston.format.timestamp()       
+        winston.format.timestamp()
     );
+
+    const customLevels = {
+        levels: {
+            crit: 0,
+            error: 1,
+            info: 2,
+            debug: 3
+        },
+        colors: {
+            crit: 'red',
+            error: 'red',
+            info: 'green',
+            debug: 'blue'
+        }
+    };
 
     let logger = winston.createLogger({
         level: env === 'development' ? 'debug' : 'info',
+        levels: customLevels.levels,
         transports: [
             new winston.transports.Console({
                 level: config.level,
                 format: consoleFormat,              
                 colorize: true,
+                timestamp: true
             })
         ]
-    });    
+    });
     return logger;
 }
 
